70c5a072d28acf41edc9f7de139a6b5114545975
smirithika <smirithichandrasegar@gmail.com>
2019-10-01 11:48:44 +0530

    Updates

---
+++ precache-manifest.75777decc9df9ffa9aba40390221a2c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2c9946489b4e046b8b093cc730fba6",
+    "url": "/React-Tic-Tac-Toe-Game/index.html"
+  },
+  {
+    "revision": "abdbf2b109a2975b4e34",
+    "url": "/React-Tic-Tac-Toe-Game/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "063c322f57c72e0d15e8",
+    "url": "/React-Tic-Tac-Toe-Game/static/js/2.ffd3ed2b.chunk.js"
+  },
+  {
+    "revision": "abdbf2b109a2975b4e34",
+    "url": "/React-Tic-Tac-Toe-Game/static/js/main.c2f9d4d9.chunk.js"
+  },
+  {
+    "revision": "c65ed7534035797b1b35",
+    "url": "/React-Tic-Tac-Toe-Game/static/js/runtime-main.b4d8cfcc.js"
+  }
+]);
